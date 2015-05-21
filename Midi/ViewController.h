d725eab6f2830b1a6b1c57b595ac86dae77c42b4
Felix <myflytop@gmail.com>
2015-05-21 16:41:46 +0800

    Start Add Bluno

--- Midi/ViewController.h
+++ Midi/ViewController.h
@@ -11,5 +11,7 @@
 @interface ViewController : UIViewController
 
 
+- (IBAction)connectAction:(id)sender;
+
 @end
 
--- Midi/ViewController.m
+++ Midi/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "MHRotaryKnob.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet MHRotaryKnob *rotaryKnob;
 
 @end
 
@@ -17,6 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self initMyknob];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +28,28 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)initMyknob{
+    self.rotaryKnob.interactionStyle = MHRotaryKnobInteractionStyleRotating;
+    self.rotaryKnob.scalingFactor = 1.5;
+    self.rotaryKnob.maximumValue = 999;
+    self.rotaryKnob.minimumValue = 0;
+    self.rotaryKnob.value = 499;
+    self.rotaryKnob.defaultValue = self.rotaryKnob.value;
+    self.rotaryKnob.resetsToDefault = YES;
+    self.rotaryKnob.backgroundColor = [UIColor clearColor];
+    [self.rotaryKnob setKnobImage:[UIImage imageNamed:@"Knob"] forState:UIControlStateNormal];
+    self.rotaryKnob.knobImageCenter = CGPointMake(100, 100);
+    [self.rotaryKnob addTarget:self action:@selector(rotaryKnobDidChange) forControlEvents:UIControlEventValueChanged];
+//    self.rotaryKnob.enabled = false;
+}
+
+- (IBAction)rotaryKnobDidChange
+{
+//    self.label.text = [NSString stringWithFormat:@"%.3f", self.rotaryKnob.value];
+//    self.slider.value = self.rotaryKnob.value;
+}
+
+
+- (IBAction)connectAction:(id)sender {
+}
 @end

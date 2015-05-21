439d150fed770304b04d2c42e20aeb5b10cd23bb
Felix <myflytop@gmail.com>
2015-05-21 22:56:19 +0800

    Finish Ble Class

--- Midi/ViewController.h
+++ Midi/ViewController.h
@@ -7,11 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DFBlunoManager.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<DFBlunoDelegate>
+@property(strong, nonatomic) DFBlunoManager* blunoManager;
+@property(strong, nonatomic) DFBlunoDevice* blunoDev;
+@property(strong, nonatomic) NSMutableArray* aryDevices;
+
+@property (strong, nonatomic) IBOutlet UIView *connectView;
+@property (strong, nonatomic) IBOutlet UIView *mainView;
+@property (strong, nonatomic) IBOutlet UIView *effectsView;
+@property (strong, nonatomic) IBOutlet UIView *menuView;
+
+@property (strong, nonatomic) IBOutlet UIButton *mtBtn;
+@property (strong, nonatomic) IBOutlet UIButton *seBtn;
 
 
 - (IBAction)connectAction:(id)sender;
+- (IBAction)playMT1:(id)sender;
+- (IBAction)playMT2:(id)sender;
+- (IBAction)playMT3:(id)sender;
+
+- (IBAction)toMTView:(id)sender;
+- (IBAction)toSEView:(id)sender;
 
 @end
 
--- Midi/ViewController.m
+++ Midi/ViewController.m
@@ -10,6 +10,10 @@
 #import "MHRotaryKnob.h"
 
 @interface ViewController ()
+{
+    BOOL _isConnect;
+    NSString* _msg;
+}
 @property (weak, nonatomic) IBOutlet MHRotaryKnob *rotaryKnob;
 
 @end
@@ -18,7 +22,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.blunoManager = [DFBlunoManager sharedInstance];
+    self.blunoManager.delegate = self;
+    self.aryDevices = [[NSMutableArray alloc] init];
+    _isConnect = NO;
+    [self searchDevices];
+    
+    _menuView.hidden = YES;
+    _mainView.hidden = YES;
+    _effectsView.hidden = YES;
+    
     [self initMyknob];
     
 }
@@ -27,6 +41,36 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)playMT1:(id)sender {
+}
+
+- (IBAction)playMT2:(id)sender {
+}
+
+- (IBAction)playMT3:(id)sender {
+}
+
+- (IBAction)toMTView:(id)sender {
+    _mainView.hidden = NO;
+    _effectsView.hidden = YES;
+    [_mtBtn setEnabled:NO];
+    [_mtBtn.titleLabel setAlpha:0.6];
+    [_seBtn setEnabled:YES];
+    [_seBtn.titleLabel setAlpha:0.9];
+}
+
+- (IBAction)toSEView:(id)sender {
+    _mainView.hidden = YES;
+    _effectsView.hidden = NO;
+    [_mtBtn setEnabled:YES];
+    [_mtBtn.titleLabel setAlpha:0.9];
+    [_seBtn setEnabled:NO];
+    [_seBtn.titleLabel setAlpha:0.6];
+    
+}
+
+#pragma mark- Rotary
 
 - (void)initMyknob{
     self.rotaryKnob.interactionStyle = MHRotaryKnobInteractionStyleRotating;
@@ -40,7 +84,7 @@
     [self.rotaryKnob setKnobImage:[UIImage imageNamed:@"Knob"] forState:UIControlStateNormal];
     self.rotaryKnob.knobImageCenter = CGPointMake(100, 100);
     [self.rotaryKnob addTarget:self action:@selector(rotaryKnobDidChange) forControlEvents:UIControlEventValueChanged];
-//    self.rotaryKnob.enabled = false;
+    self.rotaryKnob.enabled = false;
 }
 
 - (IBAction)rotaryKnobDidChange
@@ -50,6 +94,109 @@
 }
 
 
+
+
+
+
+#pragma mark- DFBlunoDelegate
+
+- (void)searchDevices{
+    [self.aryDevices removeAllObjects];
+    [self.blunoManager scan];
+}
+
+-(void)bleDidUpdateState:(BOOL)bleSupported
+{
+    if(bleSupported)
+    {
+        [self.blunoManager scan];
+    }
+}
+-(void)didDiscoverDevice:(DFBlunoDevice*)dev
+{
+    BOOL bRepeat = NO;
+    for (DFBlunoDevice* bleDevice in self.aryDevices)
+    {
+        if ([bleDevice isEqual:dev])
+        {
+            bRepeat = YES;
+            break;
+        }
+    }
+    if (!bRepeat)
+    {
+        [self.aryDevices addObject:dev];
+    }
+    //    [self.tbDevices reloadData];
+}
+-(void)readyToCommunicate:(DFBlunoDevice*)dev
+{
+    self.blunoDev = dev;
+    _isConnect = YES;
+    
+}
+-(void)didDisconnectDevice:(DFBlunoDevice*)dev
+{
+    //    _statusLabel.hidden = NO;
+    _isConnect = NO;
+}
+-(void)didWriteData:(DFBlunoDevice*)dev
+{
+    
+}
+
+-(void)didReceiveData:(NSData*)data Device:(DFBlunoDevice*)dev
+{
+    _msg = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"%@",_msg);
+//    BOOL isa0 = ([_msg rangeOfString:@"a0"].location !=NSNotFound);
+//    if (isa0) {
+//        if (_audioPlayer) {
+//            if ((_audioPlayer.volume -0.1)>0) {
+//                _audioPlayer.volume -= 0.1;
+//            }else{
+//                _audioPlayer.volume = 0;
+//            }
+//            NSLog(@"_audioPlayer.volume %f",_audioPlayer.volume);
+//        }
+//    }
+}
+
 - (IBAction)connectAction:(id)sender {
+    NSInteger nCount = [self.aryDevices count];
+    for (int i = 0; i < nCount; i++) {
+        DFBlunoDevice* device   = [self.aryDevices objectAtIndex:i];
+        NSLog(@"NO: %li Device Name is %@",(long)nCount,device.name);
+        if ([device.name  isEqual: @"BlunoV2.0"]) {
+            NSLog(@"Find device in aryDevices");
+            if (self.blunoDev == nil)
+            {
+                self.blunoDev = device;
+                [self.blunoManager connectToDevice:self.blunoDev];
+            }
+            else if ([device isEqual:self.blunoDev])
+            {
+                if (!self.blunoDev.bReadyToWrite)
+                {
+                    [self.blunoManager connectToDevice:self.blunoDev];
+                }
+            }
+            else
+            {
+                if (self.blunoDev.bReadyToWrite)
+                {
+                    [self.blunoManager disconnectToDevice:self.blunoDev];
+                    self.blunoDev = nil;
+                }
+                
+                [self.blunoManager connectToDevice:device];
+            }
+            
+            //            [self setupUI];
+            _connectView.hidden = YES;
+            _mainView.hidden = NO;
+            _menuView.hidden = NO;
+        }
+    }
 }
 @end
